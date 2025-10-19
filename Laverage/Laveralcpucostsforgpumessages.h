862df3b6f4cebe2b92786758ab863698c49f024f
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-19 12:55:38 +0200

    Create Laveralcpucostsforgpumessages.h

---
+++ Laverage/Laveralcpucostsforgpumessages.h
@@ -0,0 +1,8 @@
+//the big cpu could enhance gpu respondes
+//costs too. then, the timelpase between cache entries responses 
+//and chips response commands could be set to 12eV. 
+//the interlediate level could have a nowadays behaviour.
+//then fpga is well suited to emulate lower energy comma'ds devices.
+//it means that we can use new ard siks texhnologies.
+//but to nejance it, we need to tinier cpu which is not.the case here.
+//so, lets zdd hds latent space unsecure checks.
